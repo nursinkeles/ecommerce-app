51edd0355fb96e26f4bd2f4de27930a74aed121f
unknown <nur-sin_sena@hotmai.com>
2022-02-15 15:48:53 +0300

    profile page has been added

--- client/src/pages/Profile/index.js
+++ client/src/pages/Profile/index.js
@@ -1,6 +1,28 @@
-import React from "react";
-function Profile() {
-  return <div></div>;
+import { useAuth } from "../../contexts/AuthContext";
+
+import { Text, Button } from "@chakra-ui/react";
+
+function Profile({ history }) {
+  const { user, logout } = useAuth();
+
+  const handleLogout = async () => {
+    logout(() => {
+      history.push("/");
+    });
+  };
+
+  return (
+    <div>
+      <Text fontSize="22">Profile</Text>
+      <code>{JSON.stringify(user)}</code>
+
+      <br />
+      <br />
+      <Button colorScheme="pink" variant="solid" onClick={handleLogout}>
+        Logout
+      </Button>
+    </div>
+  );
 }
 
 export default Profile;
